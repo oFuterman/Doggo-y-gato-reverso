a6ceac1d76d43feb650cccfa26fdc14cc90da5c6
Dylan Widjaja <dwidjaja945@gmail.com>
2018-04-04 10:30:16 -0700

    added variables for adjacent squares in reference to any square

--- main.js
+++ main.js
@@ -1,7 +1,32 @@
 /*-----------------Dylan's Code-----------------*/
 
+function determineValidMove() {
+    /* Location of adjacent pieces */
+    var diagonalUpLeft = (parseInt(/*$(div of square)*/.attr("squareNumber")) - 9);
+    var diagonalUpRight = (parseInt(/*$(div of square)*/.attr("squareNumber")) - 7);
+    var abovePiece = (parseInt(/*$(div of square)*/.attr("squareNumber")) - 8));
+    var rightPiece = (parseInt(/*$(div of square)*/.attr("squareNumber")) + 1));
+    var leftPiece = (parseInt(/*$(div of square)*/.attr("squareNumber")) - 1));
+    var diagonalDownLeft = (parseInt(/*$(div of square)*/.attr("squareNumber")) + 7));
+    var diagonalDownRight = (parseInt(/*$(div of square)*/.attr("squareNumber")) + 9));
+    var belowPiece = (parseInt(/*$(div of square)*/.attr("squareNumber")) + 8));
 
 
+
+}
+
+
+
+
+
+
+
+
+
+
+function resetGame() {
+
+}
 
 
 
