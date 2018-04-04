6408ee7294e44469b0a86a7f82286cbf35f5acaa
Omer <omerfu@gmail.com>
2018-04-04 10:38:41 -0700

    test function

--- main.js
+++ main.js
@@ -29,3 +29,22 @@
 
 
 /*-----------------Omer's Code-----------------*/
+
+$(document).ready(initializeApp);
+
+function initializeApp(){
+    clickTest();
+}
+
+function clickTest(){
+    $('.square').on('click',function(){
+        if($(this > 'div').hasClass('white')){
+            $(this > 'div').removeClass('white');
+            $(this > 'div').addClass('black');
+        }else if($(this > 'div').hasClass('black')){
+            $(this > 'div').removeClass('black');
+        }else{
+            $(this > 'div').addClass('white')
+        }
+    });
+}
