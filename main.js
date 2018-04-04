854195ec0f0216464d10244456031a6729e92a77
Dylan Widjaja <dwidjaja945@gmail.com>
2018-04-04 12:27:17 -0700

    changed variables for adjacent squares into a gameBoardArray

--- main.js
+++ main.js
@@ -8,24 +8,52 @@ function determineValidMove() {
     /* Location of adjacent pieces */
     var squareNumber =  parseInt($(this).attr("squareNumber"));
 
-    var diagonalUpLeft = (squareNumber - 9);
-    var diagonalUpRight = (squareNumber - 7);
-    var abovePiece = (squareNumber - 8);
-    var rightPiece = (squareNumber + 1);
-    var leftPiece = (squareNumber - 1);
-    var diagonalDownLeft = (squareNumber + 7);
-    var diagonalDownRight = (squareNumber + 9);
-    var belowPiece = (squareNumber + 8);
+    var gameBoardArray =
+        [
+            [0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0],
+            [0,0,0,1,2,0,0,0],
+            [0,0,0,2,1,0,0,0],
+            [0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0]
+    ];
 
+    // var diagonalUpLeft = (squareNumber - 9);
+    // var diagonalUpRight = (squareNumber - 7);
+    // var abovePiece = (squareNumber - 8);
+    // var rightPiece = (squareNum`ber + 1);
+    // var leftPiece = (squareNumber - 1);
+    // var diagonalDownLeft = (squareNumber + 7);
+    // var diagonalDownRight = (squareNumber + 9);
+    // var belowPiece = (squareNumber + 8);
 
+    //Player 1 turn (black, 1)
 
+    for(var y = 0; y < 8; y++) {
+        for(var x = 0; x<8; x++) {
+            if(gameBoardArray[x][y] === 1) {
+                if (gameBoardArray[x-1][y] === 0) {
 
-}
-
-function removeClickHandlers() {
-    if($(".square").hasClass("clicked")) {
-
+                }
+            }
+        }
     }
+
+//     Determine legal move for black:
+// // look through entire grid until find a piece
+// // check color of piece
+// // if white,
+//     Look at all piece bottom right until black piece
+//     If top left(opposite direction) empty,
+//         Add click handler to top left
+//     Repeat for all directions
+
+
+
+
+
 }
 
 function addClickHandlerTest() {
