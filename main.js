6db7683bcf0a83fd40b0e07b612c14e8a3cb3584
Dylan Widjaja <dwidjaja945@gmail.com>
2018-04-04 11:19:59 -0700

    tested functionality of variables to pull numeric value of squares

--- main.js
+++ main.js
@@ -1,21 +1,30 @@
 /*-----------------Dylan's Code-----------------*/
 
+$(document).ready(function() {
+    addClickHandlerTest();
+});
+
 function determineValidMove() {
     /* Location of adjacent pieces */
-    var diagonalUpLeft = (parseInt(/*$(div of square)*/.attr("squareNumber")) - 9);
-    var diagonalUpRight = (parseInt(/*$(div of square)*/.attr("squareNumber")) - 7);
-    var abovePiece = (parseInt(/*$(div of square)*/.attr("squareNumber")) - 8));
-    var rightPiece = (parseInt(/*$(div of square)*/.attr("squareNumber")) + 1));
-    var leftPiece = (parseInt(/*$(div of square)*/.attr("squareNumber")) - 1));
-    var diagonalDownLeft = (parseInt(/*$(div of square)*/.attr("squareNumber")) + 7));
-    var diagonalDownRight = (parseInt(/*$(div of square)*/.attr("squareNumber")) + 9));
-    var belowPiece = (parseInt(/*$(div of square)*/.attr("squareNumber")) + 8));
+    var square =  $(this);
+    var diagonalUpLeft = (parseInt(square.attr("squareNumber")) - 9);
+    var diagonalUpRight = (parseInt(square.attr("squareNumber")) - 7);
+    var abovePiece = (parseInt(square.attr("squareNumber")) - 8);
+    var rightPiece = (parseInt(square.attr("squareNumber")) + 1);
+    var leftPiece = (parseInt(square.attr("squareNumber")) - 1);
+    var diagonalDownLeft = (parseInt(square.attr("squareNumber")) + 7);
+    var diagonalDownRight = (parseInt(square.attr("squareNumber")) + 9);
+    var belowPiece = (parseInt(square.attr("squareNumber")) + 8);
+
 
 
 
 }
 
 
+function addClickHandlerTest() {
+    $(".square").click(determineValidMove);
+}
 
 
 
