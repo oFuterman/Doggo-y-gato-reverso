3aacc29895d0798e5cbf2eab86157641c5962ca5
Omer <omerfu@gmail.com>
2018-04-04 11:03:56 -0700

    count function added

--- main.js
+++ main.js
@@ -36,15 +36,33 @@ function initializeApp(){
     clickTest();
 }
 
-function clickTest(){
-    $('.square').on('click',function(){
-        if($(this > 'div').hasClass('white')){
-            $(this > 'div').removeClass('white');
-            $(this > 'div').addClass('black');
-        }else if($(this > 'div').hasClass('black')){
-            $(this > 'div').removeClass('black');
+function clickTest(){//test function, adds or removes class of white/black
+
+    $('div[squareNumber]').on('click',function(){
+        if($(this).hasClass('white')){
+            $(this).removeClass('white');
+            $(this).addClass('black');
+        }else if($(this).hasClass('black')){
+            $(this).removeClass('black');
         }else{
-            $(this > 'div').addClass('white')
+            $(this).addClass('white')
         }
     });
+}
+
+function countPieces(){//when called returns an array with the amount of white and black pieces ordered respectively
+    var whiteCount=0;
+    var blackCount=0;
+    var squareSelector='';
+
+    for(var i=1;i<=64;i++){
+        squareSelector = 'div[squareNumber='+i+']';
+        if($(squareSelector).hasClass('white')){
+            whiteCount++;
+        }else if($(squareSelector).hasClass('black')){
+            blackCount++;
+        }
+    }
+    var pieceCountArr=[whiteCount, blackCount];
+    return pieceCountArr;
 }
