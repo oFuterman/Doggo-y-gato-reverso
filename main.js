82cb6c30d702c6ccc0245180d1b39912ef30dd82
Omer <omerfu@gmail.com>
2018-04-04 11:17:11 -0700

    test function changed

--- main.js
+++ main.js
@@ -33,21 +33,19 @@
 $(document).ready(initializeApp);
 
 function initializeApp(){
-    clickTest();
+    $('div[squareNumber]').on('click',clickTest);
 }
 
 function clickTest(){//test function, adds or removes class of white/black
-
-    $('div[squareNumber]').on('click',function(){
-        if($(this).hasClass('white')){
-            $(this).removeClass('white');
-            $(this).addClass('black');
-        }else if($(this).hasClass('black')){
-            $(this).removeClass('black');
-        }else{
-            $(this).addClass('white')
-        }
-    });
+    debugger
+    if($(this).hasClass('white')){
+        $(this).removeClass('white');
+        $(this).addClass('black');
+    }else if($(this).hasClass('black')){
+        $(this).removeClass('black');
+    }else{
+        $(this).addClass('white')
+    }
 }
 
 function countPieces(){//when called returns an array with the amount of white and black pieces ordered respectively
