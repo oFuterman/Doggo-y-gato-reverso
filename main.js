aebd77520e735785e4cf6bf5099a43d79777c5f6
Omer <omerfu@gmail.com>
2018-04-09 10:39:50 -0700

    opacity change on win modal

--- main.js
+++ main.js
@@ -387,6 +387,7 @@ function sideFlip(num, squareSelector){//takes in number and checks correspondin
 
     switch(num){
         case 0:
+            debugger
             rowChange=-1;
             colChange=-1;
             break;
